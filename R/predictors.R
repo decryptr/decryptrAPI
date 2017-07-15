8736f8820abc6e0b74f7417727f6af12cbc95bbc
Julio Trecenti <julio.trecenti@gmail.com>
2017-07-15 15:30:34 -0300

    base64 option

--- R/predictors.R
+++ R/predictors.R
@@ -15,12 +15,18 @@ print_input <- function(img) {
 #'
 #' @param img image content.
 #' @param type one of \code{rfb}, \code{trt}, \code{tjmg} or \code{esaj}.
+#' @param base64 passing base64 image as argument.
 #'
 #' @import decryptr
 #'
 #' @export
-predict_captcha <- function(img, type) {
-  eval(parse(text = sprintf('predict_%s', type)))(base64enc::base64decode(img))
+predict_captcha <- function(img, type, base64 = TRUE) {
+  if (base64) {
+    decoded <- base64enc::base64decode(img)
+  } else {
+    decoded <- decryptr::read_captcha(img)
+  }
+  eval(parse(text = sprintf('predict_%s', type)))(decoded)
 }
 
 predict_rfb <- function(img) {
@@ -35,6 +41,6 @@ predict_esaj <- function(img) {
   predict(esaj_model, newdata = decryptr::prepare(img))
 }
 
-predict_esaj <- function(img) {
+predict_tjmg <- function(img) {
   predict(tjmg_model, newdata = decryptr::prepare(img))
 }
